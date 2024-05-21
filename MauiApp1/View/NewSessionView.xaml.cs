aeeb88c5c387bee21ba28c6dea1d1d4a783613ce
Diego Estevez <diegoestevez73@gmail.com>
2024-05-20 22:10:30 -0400

    Creada pantala de insertar sesiones

--- MauiApp1/View/NewSessionView.xaml.cs
+++ MauiApp1/View/NewSessionView.xaml.cs
@@ -6,4 +6,20 @@ public partial class NewSessionView : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void entSessionName_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		Entry ent = (Entry)sender;
+
+		btnNextStep.IsEnabled = string.IsNullOrEmpty(ent.Text) ? false : true;
+    }
+
+    private void btnNextStep_Clicked(object sender, EventArgs e)
+    {
+        string sessionName = entSessionName.Text;
+        DateTime sessionDate = dpkSessionDate.Date;
+        TimeSpan sessionTime = dpkSessionTime.Time;
+
+        //string sessionLevel =;
+    }
 }
