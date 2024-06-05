3a903c6d21495da5c5c20bf53df674e4425a2a30
bkfan1 <jpf8296@gmail.com>
2024-06-04 21:54:29 -0400

    Crea interfaz para SessionMovementRepository

---
+++ MauiApp1/Repositories/ISessionMovementRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpinningTrainer.Models;
+
+namespace SpinningTrainer.Repositories
+{
+    interface ISessionMovementRepository
+    {
+        SessionMovementModel Add(SessionMovementModel sessionMovement);
+        SessionMovementModel Update(SessionMovementModel sessionMovement);
+        void Delete(int id);
+        IEnumerable<SessionMovementModel> GetBySessionID(int sessionID);
+        IEnumerable<SessionMovementModel> GetAll();
+    }
+}
