9f3bde772b1ac3cde92c2dc16c4ed2a970867c70
bkfan1 <jpf8296@gmail.com>
2024-06-04 21:55:46 -0400

    Añade Repositorio para Session

---
+++ MauiApp1/Repositories/SessionRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using SpinningTrainer.Models;
+
+namespace SpinningTrainer.Repositories
+{
+    public class SessionRepository : RepositoryBase, ISessionRepository
+    {
+        public SessionModel Add(SessionModel session)
+        {
+            return session;
+        }
+
+        public void Delete(string id)
+        {
+        }
+
+        public IEnumerable<SessionModel> GetAll()
+        {
+            return new List<SessionModel>();
+        }
+
+        public SessionModel GetByIDEntrenador(int IDEntrenador)
+        {
+            return new SessionModel();
+        }
+
+        public SessionModel Update(SessionModel sessionModel)
+        {
+            return sessionModel;
+        }
+    }
+}
