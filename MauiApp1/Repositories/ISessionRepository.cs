f4ceea4845b49b8c190fc91bba429b8c0aef4229
bkfan1 <jpf8296@gmail.com>
2024-06-04 21:54:39 -0400

    Crea interfaz para SessionRepository

---
+++ MauiApp1/Repositories/ISessionRepository.cs
@@ -0,0 +1,13 @@
+﻿using SpinningTrainer.Models;
+
+namespace SpinningTrainer.Repositories
+{
+    public interface ISessionRepository
+    {
+        SessionModel Add(SessionModel session);
+        SessionModel Update(SessionModel session);
+        void Delete(string id);
+        SessionModel GetByIDEntrenador(int IDEntrenador);
+        IEnumerable<SessionModel> GetAll();
+    }
+}
