ae8bf8b4ea2a9eefdb616428a9fee8019ede1dd1
bkfan1 <jpf8296@gmail.com>
2024-06-05 23:13:53 -0400

    Crea clase SessionExerciseModel

---
+++ MauiApp1/Models/SessionExerciseModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpinningTrainer.Models
+{
+    public class SessionExerciseModel
+    {
+        public int ID { get; set; }
+        public int IDSesion { get; set; }
+        public int IDMovimiento { get; set; }
+        public int IDPosicionMano { get; set; }
+        public short TipoEjercicio { get; set; }
+        public int Fase { get; set; }
+
+        // public int RPMMin { get; set; } // Si se requiere, descomentar esta línea
+        public int RPMMed { get; set; }
+        public int RPMFin { get; set; }
+        public int DuracionSeg { get; set; }
+
+    }
+}
