cfb0dc5ee8b99abca3f25ff738a55a6b0b616faf
bkfan1 <jpf8296@gmail.com>
2024-06-05 23:13:12 -0400

    Crea clase ExerciseModel

---
+++ MauiApp1/Models/ExerciseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpinningTrainer.Models
+{
+    public class ExerciseModel
+    {
+        public int ID { get; set; }
+        public string Descrip { get; set; }
+        public short TipoMov { get; set; }
+        public int RPMMin { get; set; }
+        // public int RPMMed { get; set; } // Si se requiere, descomentar esta línea
+        public int RPMMax { get; set; }
+        public string PosicionesDeManos { get; set; }
+    }
+}
