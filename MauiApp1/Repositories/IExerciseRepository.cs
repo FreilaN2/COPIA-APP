64dd367d00ea245954cf4fbc2f175e70ffe4b2f0
bkfan1 <jpf8296@gmail.com>
2024-06-05 23:14:41 -0400

    Crea clase lExerciseRepository

---
+++ MauiApp1/Repositories/IExerciseRepository.cs
@@ -0,0 +1,15 @@
+﻿using SpinningTrainer.Models;
+using System.Collections.Generic;
+
+
+namespace SpinningTrainer.Repositories
+{
+    public interface IExerciseRepository
+    {
+        ExerciseModel Add(ExerciseModel exercise);
+        ExerciseModel Update(ExerciseModel exercise);
+        void Delete(int id);
+        ExerciseModel GetById(int id);
+       IEnumerable<ExerciseModel> GetAll();
+    }
+}
