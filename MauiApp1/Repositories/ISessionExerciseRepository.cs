5ea7eaffcdbd83710ee6134a351fff7c1725b611
bkfan1 <jpf8296@gmail.com>
2024-06-05 23:14:54 -0400

    Crea clase ISessionExerciseRepository

---
+++ MauiApp1/Repositories/ISessionExerciseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpinningTrainer.Models;
+
+namespace SpinningTrainer.Repositories
+{
+    interface ISessionExerciseRepository
+    {
+        SessionExerciseModel Add(SessionExerciseModel sessionExercise);
+        SessionExerciseModel Update(SessionExerciseModel sessionExercise);
+        void Delete(int id);
+
+        IEnumerable<SessionExerciseModel> GetAll();
+
+        IEnumerable<SessionExerciseModel> GetAllBySessionID(int sessionID);
+        
+        SessionExerciseModel GetByID(int id);
+    }
+}
